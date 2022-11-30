acbb268a491caee8d4ed27d51d450c890c46d2c6
Andrew Read <andrew.read@ignitist.net>
2022-11-30 08:38:33 -0500

    base structure and pages

--- main/resources/scripts/script.js
+++ main/resources/scripts/script.js
@@ -1,15 +1,14 @@
-import moment from 'moment';
 const officer_line = document.getElementById('#members_main');
 const officer_line_title = document.getElementById('#officers_title');
-const year_span = document.getElementsByClassName('.year_span')
+const year_spans = document.getElementsByClassName("year_span")
 
-
-function getYear(){
-    let start_year = moment().year();
-    let end_year = moment().add('year', 1);
-    year_span.innerHTML = start_year + ' - ' + end_year;
-};
-getYear();
-
-console.log(start_year);
-
+function setYear() {
+    for (x = 0; x <= year_spans.length; x++){
+        let start_year = moment().format('YYYY');
+        let end_year = moment().add(1, 'year').format('YYYY');
+        let current_years = start_year + ' - ' + end_year;
+        year_spans[x].innerHTML = current_years;
+        console.log(current_years);
+    }
+}
+setYear();
